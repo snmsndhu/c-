bb4ee69571ff048891826bbd306b0694706b3791
Sandeep Singh <snmsndhu47@gmail.com>
2024-09-05 12:55:31 -0400

    fundamentals data types

---
+++ 01_Fundamentals/02_Fundamental_Data_Types/01_Fundamental_data_types/main.cpp
@@ -0,0 +1,10 @@
+// For storing the small number we can use SHORT.
+// For storing the Normal number we can use INT.
+// For storing the Long number we can use LONG.
+// For storing the BIG number we can use LONG LONG.
+
+// For number with decimals we have float, double, long double.
+
+// For true and false we can use bool.
+
+// For the single charcters we have char.
---
+++ 01_Fundamentals/02_Fundamental_Data_Types/02_Initializing_variables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 5;
+    double price = 99.99;
+    float interestRate = 3.67f;
+    long fileSize = 90000l;
+    char letter = 'a';
+    bool isValid = false;
+    return 0;
+}
