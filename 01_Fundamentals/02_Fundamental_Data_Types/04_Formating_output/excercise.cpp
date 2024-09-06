e263cdd57937e207835107bc85b5da8a49baf2da
Sandeep Singh <snmsndhu47@gmail.com>
2024-09-06 12:00:26 -0400

    formating output

--- 01_Fundamentals/02_Fundamental_Data_Types/04_Formating_output/excercise.cpp
+++ 01_Fundamentals/02_Fundamental_Data_Types/04_Formating_output/excercise.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << left << setw(15) << "Course" << setw(10) << "Students" << endl
-         << setw(15) << "C++" << setw(10) << 100 << endl
-         << setw(15) << "JavaScript" << setw(10) << 50 << endl;
+         << setw(15) << "C++" << setw(10) << right << 100 << left << endl
+         << setw(15) << "JavaScript" << setw(10) << right << 50 << endl;
     return 0;
 }
